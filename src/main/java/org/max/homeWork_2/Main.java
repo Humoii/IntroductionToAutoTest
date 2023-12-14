0fecd389f6ecffa123ad65c5a9c26cdb9b902be6
Сергей <sergeiik123@yandex.ru>
2023-12-14 19:37:20 +0300

    HomeWorkLesson_2

--- src/main/java/org/max/homeWork_2/Main.java
+++ src/main/java/org/max/homeWork_2/Main.java
@@ -2,31 +2,29 @@ package org.max.homeWork_2;
 
 public class Main {
     public static void main(String[] args) {
-        Apple apple1 = new Apple();
-        Banana banana1 = new Banana();
-        Orange orange1 = new Orange();
-
-        Apple apple2 = new Apple();
-        Orange orange2 = new Orange();
-        Orange orange3 = new Orange();
-        Orange orange4 = new Orange();
+        Apple apple = new Apple();
+        Banana banana = new Banana();
+        Orange orange = new Orange();
 
         FruitsBox fruitsBox = new FruitsBox();
         FruitsBox fruitsBox1 = new FruitsBox();
         FruitsBox fruitsBox2 = new FruitsBox();
 
-        fruitsBox2.addFruits(apple1);
-        fruitsBox2.addFruits(banana1);
-        fruitsBox2.addFruits(orange1);
+        fruitsBox2.addFruits(apple);
+        fruitsBox2.addFruits(banana);
+        fruitsBox2.addFruits(orange);
 
-        fruitsBox1.addFruits(apple2);
-        fruitsBox1.addFruits(orange2);
-        fruitsBox1.addFruits(orange3);
-        fruitsBox1.addFruits(orange4);
+        fruitsBox1.addFruits(apple);
+        fruitsBox1.addFruits(orange);
+        fruitsBox1.addFruits(orange);
+        fruitsBox1.addFruits(orange);
 
         fruitsBox.addFruits(fruitsBox1);
         fruitsBox.addFruits(fruitsBox2);
 
-        System.out.println(fruitsBox.fruits());
+        fruitsBox.addFruits(apple);
+        fruitsBox.addFruits(orange);
+
+        System.out.println("Количество фруктов: " + fruitsBox.fruits());
     }
 }
